cc1c4a0bc6c47d5e89fe6cb5661c522a2c40bfaf
Manish Jadhav <manish@altrr.in>
2025-10-26 13:25:28 +0530

    Reorganize project structure and add virtual controls
    - Move all game files, assets, and libraries to src/ folder
    - Add keyboard arrow controls for cow movement
    - Integrate Phaser Virtual Gamepad plugin for touch/mobile support
    - Add comprehensive JSDoc comments to Game.js
    - Configure JSDoc generation (jsdoc.json)
    - Update build configuration and ignore patterns
    - Exclude libs from documentation generation

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-import './Boot.js';
-import './Preloader.js';
-import './StartMenu.js';
-import './Game.js';
+import "./src/Boot.js";
+import "./src/Preloader.js";
+import "./src/StartMenu.js";
+import "./src/Game.js";
 
-window.onload = function() {
-	var game = new Phaser.Game(960, 540, Phaser.CANVAS, '');
-	game.state.add('Boot', feedTheCow.Boot);
-	game.state.add('Preloader', feedTheCow.Preloader);
-	game.state.start('Boot');
-	game.state.add('StartMenu', feedTheCow.StartMenu);
-	game.state.add('Game', feedTheCow.Game);
+window.onload = function () {
+  var game = new Phaser.Game(960, 540, Phaser.CANVAS, "");
+  game.state.add("Boot", feedTheCow.Boot);
+  game.state.add("Preloader", feedTheCow.Preloader);
+  game.state.start("Boot");
+  game.state.add("StartMenu", feedTheCow.StartMenu);
+  game.state.add("Game", feedTheCow.Game);
 };
